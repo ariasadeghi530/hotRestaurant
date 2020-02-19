18556aed7b8a9348ab7156d8675606b0fea1ddfc
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-19 13:05:14 -0800

    did stuff

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const fs = require('fs')
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const express = require('express');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000)
+app.listen(3000);
