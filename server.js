002f0b8d27dd611ee9f2b0a74f86ff5860f8cc67
Carlo <csnazareno94@gmail.com>
2020-02-19 13:02:07 -0800

    routes

--- server.js
+++ server.js
@@ -2,10 +2,36 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const fs = require('fs')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/fuckyou', (req,res) => {
+  fs.readFile('fuckyou.son', 'utf8', (e, data) => {
+    if (e) {console.log(e)}
+
+    const reservations = JSON.parse(data)
+
+    res.json(reservations)
+  })
+})
+
+app.post('/fuckyou', (req, res) => {
+  fs.readFile('fuckyou.json', 'utf8', (e, data) => {
+    if (e) { console.log(e) }
+
+    const employees = JSON.parse(data)
+
+    employees.push(req.body)
+
+    fs.writeFile('fuckyou.json', JSON.stringify(employees), e => {
+      if (e) { console.log(e) }
+      res.sendStatus(200)
+    })
+  })
+})
 
 app.listen(3000)
