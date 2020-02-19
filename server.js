62bf4eae43679ea9f329078f6869497c84f7e92f
Quan Huynh <qlhuynh@gmail.com>
2020-02-19 12:54:58 -0800

    initial setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const fs = require('fs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000)
